338621ff2fa9fcfe2c0228612d97068d0ba81dd8
Ember Doyle <doyle.ember@gmail.com>
2024-09-17 16:26:35 -0600

    Create check for win within gamboard factory function

--- script.js
+++ script.js
@@ -26,12 +26,36 @@ let Gameboard = (function createGameboard(){//module for creating initial empty 
                 gameboard.row3[cell] = 0;
             }
         }
-        
-        console.log("Gameboard after adding an x");
-        console.log(gameboard);
+
         return gameboard;
     }
-    return {createEmptyBoard,addMarker};
+    function checkForWinOrTie(gameboard){
+        for (const row in gameboard) {
+            gameboard.row1[1]=1; //just temp for checking this loop
+            let cell1 = `${gameboard[row][0]}`;
+            let cell2 = `${gameboard[row][1]}`;
+            let cell3 = `${gameboard[row][2]}`;
+            if(cell1==cell2&&cell1==cell3&&cell2==cell3){
+                console.log("we have a winner");
+            }
+            else{
+                console.log("this row contains "+cell1+cell2+cell3);
+            }
+            
+            //`${gameboard[row][2]}`
+            //`${gameboard[row][3]}`
+          }
+        //for
+        //check for win
+        //if any row is all 1s or 0s
+
+        //if any column is all 1s or 0s
+        //if diagonals are all 1s or 0s
+
+        //check if board is full and no win i.e tie
+
+    }
+    return {createEmptyBoard,addMarker,checkForWinOrTie};
 })();
 
 
@@ -42,16 +66,20 @@ let oneGame = (function playGame(){
     let gameboard = Gameboard.createEmptyBoard;
     let userMarker = 1;//x
     let computerMarker = 0;//o
-    let userChoice; //this will be gotten from a click but for now ill set it
+    let userOneChoice,userTwoChoice; //this will be gotten from a click but for now ill set it
   
-    //exmaple game flow for builidng game
-    Gameboard.addMarker(gameboard,userMarker,3,0);//user choice
-    Gameboard.addMarker(gameboard,computerMarker,3,2);//computer choice
-    Gameboard.addMarker(gameboard,userMarker,1,2);//user choice
-    Gameboard.addMarker(gameboard,computerMarker,2,1);//computer choice
-    Gameboard.addMarker(gameboard,userMarker,1,0);//user choice
-    Gameboard.addMarker(gameboard,computerMarker,1,1);//computer choice
-    Gameboard.addMarker(gameboard,userMarker,2,0);//user choice
+    //example game flow for builidng game - user 1 wins
+    Gameboard.addMarker(gameboard,userMarker,3,0);//user1 choice
+    Gameboard.addMarker(gameboard,computerMarker,3,2);//user2 choice
+    Gameboard.addMarker(gameboard,userMarker,1,2);//user1 choice
+    Gameboard.addMarker(gameboard,computerMarker,2,1);//user2 choice
+    Gameboard.addMarker(gameboard,userMarker,1,0);//user1 choice
+    Gameboard.addMarker(gameboard,computerMarker,1,1);//user2 choice
+    Gameboard.addMarker(gameboard,userMarker,2,0);//user1 choice
+    //check for win
+    Gameboard.checkForWinOrTie(gameboard);
+    //when 3 in a row or tie console log user1 won/user2 won/tie
+    
 
 
     return{};
